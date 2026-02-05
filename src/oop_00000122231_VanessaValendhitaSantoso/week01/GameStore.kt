1e2545daf92f6909baede29aa054d37f331cfbc9
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-05 18:34:17 +0700

    printReceipt(title = gameTitle, finalPrice = finalPrice)

--- src/oop_00000122231_VanessaValendhitaSantoso/week01/GameStore.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week01/GameStore.kt
@@ -6,10 +6,18 @@ fun main() {
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) {
     (price * 0.2).toInt()
 } else {
     (price * 0.1).toInt()
+}
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("~ STRUK PEMBELIAN STEAMKW ~")
+    println("Judul Game  : $title")
+    println("Harga Akhir : Rp $finalPrice")
 }
