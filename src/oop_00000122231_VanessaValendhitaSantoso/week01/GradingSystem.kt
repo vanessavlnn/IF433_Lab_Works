3739fd6dcdaa55e293fb30b980ac7f1a911c5fa7
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-05 14:45:31 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000122231_VanessaValendhitaSantoso.week01
+
+fun main () {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai : " + score)
+}
