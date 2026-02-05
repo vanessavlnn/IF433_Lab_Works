1f7094553f51a74622e71203c6e19220a0c85017
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-05 18:25:14 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000122231_VanessaValendhitaSantoso/week01/GameStore.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week01/GameStore.kt
@@ -3,4 +3,13 @@ package oop_00000122231_VanessaValendhitaSantoso.week01
 fun main() {
     val gameTitle: String = "Barbie"
     val price: Int = 67000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.2).toInt()
+} else {
+    (price * 0.1).toInt()
 }
