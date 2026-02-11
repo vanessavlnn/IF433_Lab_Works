01173a5f899d797dd3ad2e30e11049e2f51de34d
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-11 15:58:06 +0700

    week02: add warning validation in init block

--- src/oop_00000122231_VanessaValendhitaSantoso/week02/Student.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week02/Student.kt
@@ -4,6 +4,13 @@ class Student(
     val name: String,
     val nim: String,
     var major: String
-){
-
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
