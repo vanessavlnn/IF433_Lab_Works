a23eeca73432f0a84b9d237a6b25c1dfb58c54e6
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-11 16:31:43 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_00000122231_VanessaValendhitaSantoso.week02
+
+class Loan (
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
