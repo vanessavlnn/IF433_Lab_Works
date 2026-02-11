8354a232192553deea38e2681e03dbdbe01c173a
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-11 15:59:16 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week02/Main.kt
@@ -0,0 +1,27 @@
+package oop_00000122231_VanessaValendhitaSantoso.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
