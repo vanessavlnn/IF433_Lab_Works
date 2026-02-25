c513b293af24c61ce36e7bec696b88260ff20e2f
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-25 15:03:49 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week04/labclass/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000122231_VanessaValendhitaSantoso.week04.labclass
+
+fun main(){
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
