b5de146fd808541f02589a8c1aeca0aa848485f8
vanessavlnn <vanessavalendhita@gmail.com>
2026-02-25 15:40:52 +0700

    week04:(task 1) test ElectricCar in main

--- src/oop_00000122231_VanessaValendhitaSantoso/week04/labclass/ElectricCar.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week04/labclass/ElectricCar.kt
@@ -1,2 +1,7 @@
 package oop_00000122231_VanessaValendhitaSantoso.week04.labclass
 
+class ElectricCar(brand:String, numberOfDoors:Int, val batteryCapacity:Int): Car(brand, numberOfDoors) {
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+}
--- src/oop_00000122231_VanessaValendhitaSantoso/week04/labclass/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week04/labclass/Main.kt
@@ -11,4 +11,10 @@ fun main(){
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val eCar = ElectricCar("BYD", 4, 85)
+    eCar.accelerate()
+    eCar.honk()
+    eCar.openTrunk()
 }
