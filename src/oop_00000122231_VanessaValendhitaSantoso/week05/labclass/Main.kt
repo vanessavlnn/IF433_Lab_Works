9b0e18fb7769900a61d3ad22f7056dd986b6e366
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-04 15:10:24 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week05/labclass/Main.kt
@@ -0,0 +1,25 @@
+package oop_00000122231_VanessaValendhitaSantoso.week05.labclass
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex",  "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------")
+    }
+}
