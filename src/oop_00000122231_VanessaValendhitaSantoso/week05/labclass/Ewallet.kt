8c7cf1d6b225c3bc27c62891374c0215540a5ddc
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-04 15:31:40 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week05/labclass/Ewallet.kt
@@ -0,0 +1,17 @@
+package oop_00000122231_VanessaValendhitaSantoso.week05.labclass
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran sukses menggunakan E-Wallet. Saldo tersisa: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
