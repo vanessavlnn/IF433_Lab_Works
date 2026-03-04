dd9667c80c6fee30da460343ba952e91093d4b80
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-04 15:16:42 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/oop_00000122231_VanessaValendhitaSantoso/week05/labclass/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week05/labclass/Main.kt
@@ -1,25 +1,26 @@
 package oop_00000122231_VanessaValendhitaSantoso.week05.labclass
 
-fun main() {
-    val dosen1 = Dosen("Pak Alex",  "0123456")
-    val admin1 = Admin("Bu Siti")
+//fun main() {
+    //val dosen1 = Dosen("Pak Alex",  "0123456")
+    //val admin1 = Admin("Bu Siti")
 
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+    //val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
-    println("=== AKTIVITAS PEGAWAI ===")
-    for (pegawai in daftarPegawai) {
-        pegawai.bekerja()
+    //println("=== AKTIVITAS PEGAWAI ===")
+    //for (pegawai in daftarPegawai) {
+        //pegawai.bekerja()
 
-        when (pegawai) {
-            is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar()
-            }
-            is Admin -> {
-                println("=> Terdeteksi sebagai Admin")
-                pegawai.doAdminWork()
-            }
-        }
-        println("-------------------------")
-    }
-}
+        //when (pegawai) {
+            //is Dosen -> {
+                //println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                //pegawai.mengajar()
+            //}
+            //is Admin -> {
+                //println("=> Terdeteksi sebagai Admin")
+                //pegawai.doAdminWork()
+            //}
+        //}
+        //println("-------------------------")
+    //}
+//}
+
