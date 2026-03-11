c80dcca58cccaecd27c8e8d5a08463da0d8a026c
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-11 15:36:50 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000122231_VanessaValendhitaSantoso.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
