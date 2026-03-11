e27a10138626584bca020120c391c4baab2781a8
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-11 15:08:06 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000122231_VanessaValendhitaSantoso/week06/Smartphone.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week06/Smartphone.kt
@@ -1,4 +1,9 @@
 package oop_00000122231_VanessaValendhitaSantoso.week06
 
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.git ")
+    }
 }
