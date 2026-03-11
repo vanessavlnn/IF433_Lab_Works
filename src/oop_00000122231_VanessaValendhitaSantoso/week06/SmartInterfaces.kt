5c83d9366c26ee3a59c33981f4d912f569736919
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-11 15:29:45 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week06/SmartInterfaces.kt
@@ -0,0 +1,18 @@
+package oop_00000122231_VanessaValendhitaSantoso.week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
