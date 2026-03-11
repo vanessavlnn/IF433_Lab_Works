de16c827b9018ecb180f71d2776fe917e930a865
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-11 15:04:13 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000122231_VanessaValendhitaSantoso.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
