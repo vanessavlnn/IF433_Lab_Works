df4db9a68a837d0e76089c53a37565eafa8f7c9a
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-11 15:20:36 +0700

    Week06: implement decoupled processCheckout in main

--- src/oop_00000122231_VanessaValendhitaSantoso/week06/Smartphone.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week06/Smartphone.kt
@@ -4,6 +4,6 @@ class Smartphone : Camera, Phone {
     override fun turnOn() {
         super<Camera>.turnOn()
         super<Phone>.turnOn()
-        println("Sistem operasi Smartphone berhasil booting.git ")
+        println("Sistem operasi Smartphone berhasil booting.")
     }
 }
