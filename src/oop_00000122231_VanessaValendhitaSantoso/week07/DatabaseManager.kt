e77be9210379a1a70509f9480e24d0ae5f1abefb
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-25 15:22:32 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_00000122231_VanessaValendhitaSantoso.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready")
+    }
+}
