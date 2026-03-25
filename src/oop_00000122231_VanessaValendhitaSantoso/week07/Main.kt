5f83da3d10162a846b0a9937aad8058f906c44eb
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-25 16:36:48 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000122231_VanessaValendhitaSantoso/week07/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week07/Main.kt
@@ -5,4 +5,10 @@ fun main() {
     GameManager.startGame() // Test duplikasi
 
     println("---")
+
+    println("Drop chance Legendary: ${ItemRarity.LEGENDARY.dropChance}%")
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata awal: ${starterWeapon.item.name}, Damage: ${starterWeapon.item.damage}")
+
+    println("---")
 }
