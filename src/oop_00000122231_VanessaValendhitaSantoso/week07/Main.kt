6ff5b62fee0a0a0f5def2d110b1069917eada408
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-25 16:36:01 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000122231_VanessaValendhitaSantoso/week07/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week07/Main.kt
@@ -1,40 +1,8 @@
 package oop_00000122231_VanessaValendhitaSantoso.week07
 
 fun main() {
-    println("=== TEST SINGLETON ===")
-    println("Status : ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
+    GameManager.startGame()
+    GameManager.startGame() // Test duplikasi
 
-    println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()
-    client.connect()
-
-    println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
-
-    println("\n=== TEST DATA CLASS ===")
-    val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-    println(data1)
-    println("Sama? ${data1 == data2}")
-
-    val data3 = data1.copy(age = 23)
-    println("Hasil copy : $data3")
-
-    val (userName, userAge) = data1
-    println("Destructed: $userName berumur $userAge")
-
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    val uiMessage = when(response) {
-        is ApiResponse.Success -> "Tampilkan : ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert : ${response.message}"
-        is ApiResponse.Loading -> "Tampilkan Spinner"
-
-    }
-    println(uiMessage)
+    println("---")
 }
