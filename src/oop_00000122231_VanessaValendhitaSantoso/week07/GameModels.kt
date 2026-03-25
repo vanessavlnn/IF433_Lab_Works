392480329a142c86ee3e967be1245311288192bc
vanessavlnn <vanessavalendhita@gmail.com>
2026-03-25 16:28:44 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000122231_VanessaValendhitaSantoso.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(30),
+    RARE(15),
+    EPIC(5),
+    LEGENDARY(1)
+}
