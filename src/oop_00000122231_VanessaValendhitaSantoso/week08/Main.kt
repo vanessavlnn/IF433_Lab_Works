4a0360b4aaa1515767c091dbdf0e18276963dae1
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:55:34 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -61,4 +61,6 @@ fun main() {
     val statusLength = javaResponse!!.length
     println("Status dari Java: $javaResponse (Length: $statusLength)")
 
+    runMockUnitTest()
+
 }
---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/UserTest.kt
@@ -0,0 +1,17 @@
+package oop_00000122231_VanessaValendhitaSantoso.week08
+
+object DatabaseMock {
+    fun findUser(id: Int): UserProfile? {
+        return if (id == 1) UserProfile("TestUser", "test@test.com") else null
+    }
+}
+
+fun runMockUnitTest() {
+    println("\n=== RUNNING UNIT TEST ===")
+    val testUser = DatabaseMock.findUser(1)
+
+    val initial = testUser!!.name.substring(0, 1)
+
+    check(initial == "T") { "Test Failed! Initial is wrong." }
+    println("Test Passed: Initial is T")
+}
