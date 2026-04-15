9abedafe178e8f1355192fb0838770a9760d5871
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:49:48 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000122231_VanessaValendhitaSantoso.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
