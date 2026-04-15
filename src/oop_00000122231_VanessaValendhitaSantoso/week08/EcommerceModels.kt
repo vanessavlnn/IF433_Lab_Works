2e8bfefc4b744483149b99344ef0eb86f2ce0eb2
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 16:01:18 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000122231_VanessaValendhitaSantoso.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
