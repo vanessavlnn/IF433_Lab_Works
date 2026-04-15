7e475a94718ef13cb05ffdd6c0bc8be8434ffae8
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:51:24 +0700

    week08: implement java interop using !!

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -56,4 +56,9 @@ fun main() {
         println(e.message)
     }
 
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
+
 }
