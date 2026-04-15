bce7f0e815590b0d01ff9d7dfe1e9753de19f4ee
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:36:31 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -27,4 +27,12 @@ fun main() {
     4500000.0
     )
 
+    for (item in mixedData) {
+        val text = item as? String
+
+        text?.let{
+            println("Ditemukan teks : ${it.uppercase()}")
+        }
+    }
+
 }
