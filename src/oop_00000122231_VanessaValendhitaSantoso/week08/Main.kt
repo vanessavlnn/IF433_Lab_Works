39a1599fe215e7d10a7ce3551e892e990a9a6579
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:34:29 +0700

    week08: create mixed data collection

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -18,4 +18,13 @@ fun main() {
 
     println(receipt)
 
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+    "Smartphone",
+    1500000,
+    UserProfile("Andi", null),
+    "Laptop",
+    4500000.0
+    )
+
 }
