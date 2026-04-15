499bd7b686272a3527dd036d1f561dcb7c4d444d
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:25:56 +0700

    week08: implement safe call with let block

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -4,7 +4,18 @@ fun main() {
     println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
 
-    val destination = emptyOrder.deliveryDetails?.address?.city.name ?: "Kota Tidak Diketahui"
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
 
     println("Tujuan pengiriman $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga : RP$price, Pajak : Rp$tax"
+    } ?: "Transaksi Invalid : Harga belum di-set!"
+
+    println(receipt)
+
 }
