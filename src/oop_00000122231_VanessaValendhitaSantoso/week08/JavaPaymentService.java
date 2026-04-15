a9c875d40caf3f0b81662bde618d300e090983d7
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 16:04:59 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000122231_VanessaValendhitaSantoso.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
