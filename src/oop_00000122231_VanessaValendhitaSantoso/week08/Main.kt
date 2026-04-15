7042ea76e3e7cfec58c731f297eb726bb3594879
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-15 15:46:25 +0700

    week08: (trial) trigger NullPointerException with !!

--- src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week08/Main.kt
@@ -39,4 +39,12 @@ fun main() {
     val safeString = someObject as? String ?: "Unknwon String"
     println("Hasil cast + fallback: $safeString")
 
+    println("\n===TEST THE RED BUTTON (!!) ===")
+    val toxicData: String? = null
+    try {
+        val length =  toxicData!!.length
+    } catch (e: NullPointerException) {
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
+    }
+
 }
