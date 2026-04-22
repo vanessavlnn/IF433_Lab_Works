bbb692ede251170e857b01c38d0bd7f84e940ced
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-22 15:33:00 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_00000122231_VanessaValendhitaSantoso/week09/HigherOrderFunctions.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week09/HigherOrderFunctions.kt
@@ -7,4 +7,10 @@ fun main() {
 
     val evens = numbers.filter { it % 2 == 0 }
     println("Evens only: $evens")
+
+    println("\n=== HOF: MAP ===")
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
 }
