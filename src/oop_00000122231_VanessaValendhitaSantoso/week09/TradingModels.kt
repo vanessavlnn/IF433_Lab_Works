f3bf07b1a25925a00d40839a8822a2c0515a4a0a
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-22 15:52:24 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000122231_VanessaValendhitaSantoso.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
