fc6ff3d6b81f06748833c54bbccb236da1cd5e37
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-22 16:01:59 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000122231_VanessaValendhitaSantoso/week09/TradingDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week09/TradingDashboard.kt
@@ -13,11 +13,17 @@ fun main() {
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
     val winningTrades = closedTrades.filter { it.roe > 0 }
     val losingTrades = closedTrades.filter { it.roe <= 0 }
+
     val topPerformersString = winningTrades
         .sortedByDescending { it.roe }
         .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
     val worstPerformersString = losingTrades
         .sortedBy { it.roe }
         .map { "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
     val uniquePairs = tradeHistory.map { it.pair }.toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println("Honor Roll: $it") }
 }
