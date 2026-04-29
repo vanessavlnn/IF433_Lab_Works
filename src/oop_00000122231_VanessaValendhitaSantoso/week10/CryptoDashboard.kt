9077151307f9eaaf5da1c77a16bbf7bf1fb756b9
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-29 15:58:29 +0700

    week10: (task) populate coin repository

--- src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000122231_VanessaValendhitaSantoso.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 500.0))
 }
