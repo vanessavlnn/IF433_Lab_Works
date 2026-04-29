6809862cc9ad71c2aaf63a2181397bc1cfd7a8db
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-29 15:59:22 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
