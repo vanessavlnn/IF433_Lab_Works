0964844f716679043d529a7a6a7a31f064cb29bb
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-29 15:50:01 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000122231_VanessaValendhitaSantoso.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
