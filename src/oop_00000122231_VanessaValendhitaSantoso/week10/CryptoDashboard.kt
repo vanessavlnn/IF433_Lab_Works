5df4ea6c6c68e6635fb454266ac264c0c733c741
vanessavlnn <vanessavalendhita@gmail.com>
2026-04-29 16:00:20 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week10/CryptoDashboard.kt
@@ -13,4 +13,13 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin: ${coin.name} | Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 0.5))
+    txRepo.add(Transaction("TX002", 150.0))
+
+    println("\n--- Transaction List ---")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id} | Amount: ${tx.amount}")
+    }
 }
