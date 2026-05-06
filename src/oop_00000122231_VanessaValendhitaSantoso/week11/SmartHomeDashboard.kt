8f2290a90123931894df5cf8eb11d4ec9f2b12a8
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:31:01 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
@@ -2,10 +2,19 @@ package oop_00000122231_VanessaValendhitaSantoso.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
     SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
     }.also {
+        homeDevices.add(it)
+    }
+
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
 }
