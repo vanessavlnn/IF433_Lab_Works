a007a974737ec8ac97ba91f20ee0dfeeb423b9c8
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:05:59 +0700

    week11: test extension functions in main

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000122231_VanessaValendhitaSantoso.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullorEmptyCustom()}")
+}
