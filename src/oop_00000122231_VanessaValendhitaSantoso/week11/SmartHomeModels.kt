2f1e49460ec9ab6acfa577db342069a40679efd1
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:27:39 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000122231_VanessaValendhitaSantoso.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
