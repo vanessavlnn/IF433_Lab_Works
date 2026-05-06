b68e8338495ab19aad364560f8bda1c2bb29ed4f
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:02:04 +0700

    week11: create string extension functions

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000122231_VanessaValendhitaSantoso.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
