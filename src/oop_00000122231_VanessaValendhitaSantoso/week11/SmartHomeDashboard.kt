045c1f1ff2cfe44978ac512a9b15894633562958
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:33:30 +0700

    week11: (task) calculate total power usage

--- src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
@@ -34,4 +34,7 @@ fun main() {
     with(homeDevices) {
         println("Jumlah perangkat di dashboard: ${this.size}")
     }
+
+    val totalPower = homeDevices.run { sumOf { it.powerLoad } }
+    println("Total penggunaan daya: $totalPower Watt")
 }
