e82b7a9da32624023ae0b033fb56634e0ce07ea4
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-06 15:31:52 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week11/SmartHomeDashboard.kt
@@ -17,4 +17,12 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+
+    val acInverter = run {
+        val device = SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+        device // return objek
+    }
+    homeDevices.add(acInverter)
+
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 }
