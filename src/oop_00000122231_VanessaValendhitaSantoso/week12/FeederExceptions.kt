9b26cbe45e6795b36b57ceb8c160aae590690329
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-13 16:02:08 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_00000122231_VanessaValendhitaSantoso/week12/FeederExceptions.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week12/FeederExceptions.kt
@@ -1,3 +1,9 @@
 package oop_00000122231_VanessaValendhitaSantoso.week12
 
 sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
