a1433e599616e833b54063085b18e6839c1944d0
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-13 16:03:18 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week12/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_00000122231_VanessaValendhitaSantoso.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammer: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
