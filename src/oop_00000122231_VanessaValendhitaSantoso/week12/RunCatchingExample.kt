756d6afcb07e6a8a1df6ba3cb42a67bf5ef429a4
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-13 15:51:09 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000122231_VanessaValendhitaSantoso.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
