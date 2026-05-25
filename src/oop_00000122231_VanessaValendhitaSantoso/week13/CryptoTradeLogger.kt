69f9b5e057e23316f3d33fc6a96593e28ed882de
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-25 10:29:32 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week13/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_00000122231_VanessaValendhitaSantoso.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
