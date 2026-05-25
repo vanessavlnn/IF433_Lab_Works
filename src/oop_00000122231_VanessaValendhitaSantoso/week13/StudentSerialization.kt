338aba03b23cf65d97bf357e45404e3146246867
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-25 10:20:04 +0700

    week13: create Student model

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week13/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_00000122231_VanessaValendhitaSantoso.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
