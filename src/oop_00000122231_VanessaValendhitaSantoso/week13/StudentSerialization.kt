976b705fc69a43450bea0e2cc278032fb59c9245
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-25 10:22:47 +0700

    week13: create save and load list functions

--- src/oop_00000122231_VanessaValendhitaSantoso/week13/StudentSerialization.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week13/StudentSerialization.kt
@@ -11,3 +11,16 @@ fun fromCsv(line: String): Student {
     val parts = line.split(",")
     return Student(name = parts[0], age = parts[1].toInt(), gpa = parts[2].toDouble())
 }
+
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") { it.toCsv() })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
