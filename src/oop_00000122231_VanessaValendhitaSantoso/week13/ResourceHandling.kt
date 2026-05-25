5cd7f04c6f77e76b28333a0e8cee5e235f491b49
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-25 10:14:22 +0700

    week13: implement printWriter with safe use block

--- src/oop_00000122231_VanessaValendhitaSantoso/week13/ResourceHandling.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week13/ResourceHandling.kt
@@ -1,5 +1,7 @@
 package oop_00000122231_VanessaValendhitaSantoso.week13
 
+import java.io.File
+
 fun main() {
     println("=== TEST UNSAFE RESOURCE HANDLING ===")
     val unsafeFile = File("unsafe_logs.txt")
@@ -10,4 +12,14 @@ fun main() {
 
     writer.close()
     println("Proses penulisan unsafe selesai.")
+
+    println("\n=== TEST SAFE RESOURCE HANDLING ===")
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK.")
+        }
+    }
+    println("100 baris log berhasil di-generate dengan sangat aman.")
 }
