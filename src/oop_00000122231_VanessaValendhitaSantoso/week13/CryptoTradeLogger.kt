a69595973f3b976717a4841d4fef430f3889bba0
vanessavlnn <vanessavalendhita@gmail.com>
2026-05-25 10:34:09 +0700

    week13: (task) test full crypto trade logger pipeline

--- src/oop_00000122231_VanessaValendhitaSantoso/week13/CryptoTradeLogger.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week13/CryptoTradeLogger.kt
@@ -55,4 +55,7 @@ fun main() {
 
     val loadedData = loadTrades("crypto_trades.csv")
     val totalPnl = loadedData.sumOf { it.pnl }
+
+    loadedData.forEach { println(it) }
+    println("==== TOTAL PnL BERSIH: $totalPnl ====")
 }
