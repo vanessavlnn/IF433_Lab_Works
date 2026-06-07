5bd291ba6249b050db3f4a37d59fde9fb8766b22
vanessavlnn <vanessavalendhita@gmail.com>
2026-06-07 09:21:48 +0700

    week13: implement basic writeText function

--- src/oop_00000122231_VanessaValendhitaSantoso/week14/SrpViolation.kt
+++ src/oop_00000122231_VanessaValendhitaSantoso/week14/SrpViolation.kt
@@ -1,2 +1,17 @@
 package oop_00000122231_VanessaValendhitaSantoso.week14
 
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager {
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users VALUES ('${user.name}', '${user.email}')")
+    }
+
+    fun sendWelcomeEmail(user: User) {
+        println("Sending email to: ${user.email}")
+    }
+}
