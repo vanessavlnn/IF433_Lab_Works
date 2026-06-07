5db9566292645a43f67827b5807aae01803e0865
vanessavlnn <vanessavalendhita@gmail.com>
2026-06-07 09:42:08 +0700

    week14: (task) copy bad E-Commerce OrderProcessor code

---
+++ src/oop_00000122231_VanessaValendhitaSantoso/week14/EcommerceTask.kt
@@ -0,0 +1,21 @@
+package oop_00000122231_VanessaValendhitaSantoso.week14
+
+import java.io.File
+
+class BadOrderProcessor {
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
